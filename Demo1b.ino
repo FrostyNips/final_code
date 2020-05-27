d3b13a23fed9c33aab4e5a2f507e6c2a421f6728
FrostyNips <CrawlCo@hotmail.com>
2020-05-27 19:32:39 +1000

    Update Demo1b.ino

--- Demo1b.ino
+++ Demo1b.ino
@@ -1,13 +1,18 @@
-  #define F_CPU 16000000UL
+ #define F_CPU 16000000UL
 
-#include <QTRSensors.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "Motors.h"
 #include "Sensors.h"
 
 
+
 #define THRESHOLD 100
+#define THRESHOLD_redlower 35
+#define THRESHOLD_redupper 50
+#define THRESHOLD_purplelower 20
+#define THRESHOLD_purpleupper 35
 #define FULL_SPEED 256
 #define STOP 0
 #define HALF_SPEED FULL_SPEED/2
@@ -17,6 +22,7 @@ int i;
 const int ADC_CHANNELS = 13;
 int var[ADC_CHANNELS];
 int sensor[8];
+
 
 int main()
 {
@@ -36,66 +42,105 @@ int main()
   
   while(1){
 
-   //sensor[1] = var[8] //sensor 1
+   sensor[1] = var[9]; //sidemarker left
    sensor[2] = var[6]; //sensor 2
-   sensor[3] = var[7]; //sensor 3
+   //sensor[3] = var[7]; //sensor 3
    sensor[4] = var[8]; //sensor 4
    sensor[5] = var[11]; //sensor 5
-   sensor[6] = var[10]; //sensor 6
-   sensor[7] = var[9]; //sensor 7
-   //sensor[8] = var[] //sensor 8
-
-
+   //sensor[6] = var[]; //sensor 6
+  sensor[7] = var[10]; //sensor 7
+   sensor[8] = var[5]; //sidemarker right
+  bool stop = false;
+  
    // Going Straight
-     if (sensor[4]>THRESHOLD && sensor[5]>THRESHOLD && sensor[3]<THRESHOLD && sensor[6]<THRESHOLD ) {
-      motorSpeeds[0] = 120; // 120
-      motorSpeeds[1] = 120; // 120
+     if (sensor[4]>THRESHOLD && sensor[5]>THRESHOLD && sensor[2]<THRESHOLD && sensor[7]<THRESHOLD&& !stop) {
+      
+      motorSpeeds[0] = 105; // 120
+      motorSpeeds[1] = 90; // 120
+      if (sensor[8]>THRESHOLD){
+        
+        
+        stop = true; 
+        
+        
+     }     
+//      if (sensor[1]>THRESHOLD_purplower && sensor[1]<THRESHOLD_purpupper) {
+//      motorSpeeds[0] = 90; // 120
+//      motorSpeeds[1] = 90; // 120 }
+//      if (sensor[1]>THRESHOLD_redlower && sensor[1]<THRESHOLD_redupper) {
+//      motorSpeeds[0] = 130; // 120
+//      motorSpeeds[1] = 130; // 120
+//     }
+//     }
+     
      }
      
      // Cross-Road
-      else if (sensor[2]>THRESHOLD && sensor[3]>THRESHOLD && sensor[4]>THRESHOLD && sensor[5]>THRESHOLD && sensor[6]>THRESHOLD && sensor[7]>THRESHOLD) {
-      motorSpeeds[0] = 120; //120
-      motorSpeeds[1] = 120; //120
+      else if (sensor[2]>THRESHOLD && sensor[4]>THRESHOLD && sensor[5]>THRESHOLD && sensor[7]>THRESHOLD && !stop) {
+      motorSpeeds[0] = 130;//120
+      motorSpeeds[1] = 130; //120
       }
+
+      // Purple
+//     else if (sensor[1]>THRESHOLD_purplelower && sensor[1]<THRESHOLD_purpleupper) {
+//      motorSpeeds[0] = 90; // 120
+//      motorSpeeds[1] = 90; // 120
+//     }
+
+    //    Red
+//     else if (sensor[1]>THRESHOLD_redlower && sensor[1]<THRESHOLD_redupper) {
+//      motorSpeeds[0] = 130; // 120
+//      motorSpeeds[1] = 130; // 120
+//     }
+
+    
      
      //Turn Right
-     else if (sensor[4]<THRESHOLD&&sensor[5]>THRESHOLD||sensor[6]>THRESHOLD)  {
+     else if (sensor[4]<THRESHOLD&&sensor[5]>THRESHOLD&& !stop)  {
          
-            motorSpeeds[0] = 100; // 100
-            motorSpeeds[1] = 40; // 40
+            motorSpeeds[0] = 130; // 100
+            motorSpeeds[1] = 10; // 40
      }
-     else if (sensor[4]<THRESHOLD && sensor[5]<THRESHOLD && sensor[7]>THRESHOLD||sensor[6]>THRESHOLD)  {
+     else if (sensor[2]<THRESHOLD&&sensor[7]>THRESHOLD&& !stop)  {
        
-          motorSpeeds[0] = 100; //100
+          motorSpeeds[0] = 150; //100
           motorSpeeds[1] = 0; //0
      }
-     else if (sensor[2]<THRESHOLD && sensor[3]<THRESHOLD && sensor[4]<THRESHOLD && sensor[5]<THRESHOLD && sensor[7]<THRESHOLD && sensor[6]<THRESHOLD) {
-          motorSpeeds[0] = 100; //100
-          motorSpeeds[1] = 0; //0
-     }
+
 
      //turn left
-     else if(sensor[5]<THRESHOLD&&sensor[4]>THRESHOLD||sensor[3]>THRESHOLD){
+     else if(sensor[5]<THRESHOLD&&sensor[4]>THRESHOLD&& !stop){
        
-          motorSpeeds[0] = 40; //40
-          motorSpeeds[1] = 100; //100
+          motorSpeeds[0] = 10; //40
+          motorSpeeds[1] = 120; //100
      }
-     else if (sensor[5]<THRESHOLD && sensor[4]<THRESHOLD && sensor[2]>THRESHOLD||sensor[3]>THRESHOLD)  {
+     else if (sensor[7]<THRESHOLD && sensor[2]>THRESHOLD&& !stop)  {
         
           motorSpeeds[0] = 0; //0
-          motorSpeeds[1] = 100; //100
+          motorSpeeds[1] = 150; //100
       }
-
-      else if (sensor[3]<THRESHOLD && sensor[4]<THRESHOLD && sensor[5]<THRESHOLD && sensor[6]<THRESHOLD && sensor[7]<THRESHOLD && sensor[2]<THRESHOLD) {
       
-      motorSpeeds[0] = STOP; //STOP
-      motorSpeeds[1] = STOP; //STOP
+      else if(sensor[8]>THRESHOLD && sensor[1]<THRESHOLD&& !stop){
+        motorSpeeds[0] = STOP; //STOP
+        motorSpeeds[1] = STOP; //STOP
+      }
+      else if (sensor[2]<THRESHOLD && sensor[4]<THRESHOLD && sensor[5]<THRESHOLD && sensor[7]<THRESHOLD && !stop ) {
+      
+        motorSpeeds[0] = STOP; //STOP
+        motorSpeeds[1] = STOP; //STOP
+      } 
+      
+
+      if (stop){
+        
+        motorSpeeds[0] = STOP; //STOP
+        motorSpeeds[1] = STOP; //STOP
       }
       setMotorSpeeds(motorSpeeds);
 
-      
-    
-  }return 0;
+     
+     }
+  return 0;
 }
    
 ISR(ADC_vect)           
